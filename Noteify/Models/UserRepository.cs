a3e7c27293e34ab786165ecea38ab97ab81b8b14
Daniel CHOROUŠ <daniel.chorous@cns.cz>
2018-05-14 15:55:07 +0200

    Initial Dapper implementation

--- Noteify/Models/UserRepository.cs
+++ Noteify/Models/UserRepository.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 using Noteify.Interfaces;
 using Microsoft.Extensions.Options;
 using Noteify.Configuration.Options;
+using Dapper;
+using System.Data.SqlClient;
 
 namespace Noteify.Models
 {
@@ -19,7 +21,11 @@ namespace Noteify.Models
         }
         public void Create(User obj)
         {
-            throw new NotImplementedException();
+            using (SqlConnection conn = new SqlConnection(_config.Value.connectionStrings.DefaultConnection))
+            {
+                string sqlCommand = "Insert into tb_Users (Username,Email,DateOfRegistration,IsActive) Values ({obj.Username},{obj.Email},{Datetime.Now},{true})";
+            }
+                throw new NotImplementedException();
         }
 
         public User GetObjectByID(int id)
